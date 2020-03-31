3e83089d7243ae6f83d0a3110606d68b1376e5c7
Toan Pham <phamdinhtoan795@gmail.com>
2020-03-31 14:48:38 +0700

    Initial Commit

---
+++ NotePad/Controller/NoteTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  NoteTableViewController.swift
+//  NotePad
+//
+//  Created by Duc Pham on 3/31/20.
+//  Copyright © 2020 Duc Pham. All rights reserved.
+//
+
+import UIKit
+
+class NoteTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+      
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     
+        return 0
+    }
+
+  
+}
